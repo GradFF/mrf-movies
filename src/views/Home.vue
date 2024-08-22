c213f49b172411b58075181ac56a062049a86daa
mrfneto <mrfneto@gmail.com>
2024-08-22 16:14:56 -0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Home page</h1>
+    <p class="mt-4">Olá! {{ user?.name }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from '@/stores'
+
+const { user } = useStore()
+</script>
+
+<style lang="scss" scoped></style>
